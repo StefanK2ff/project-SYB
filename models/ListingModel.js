3f5f2a7b5b1607668d9bd7758deebefb81f213fc
jphrubant <jphrubant@gmail.com>
2020-02-13 19:22:46 +0100

    index is under construction

--- models/ListingModel.js
+++ models/ListingModel.js
@@ -19,11 +19,11 @@ const listingSchema = new Schema(
       required: true
     },
     type: {
-      type: Array,
+      type: String,
       required: true,
       enum: ["canoe", "kayak", "rowing", "inflatable", "paddle", "boat"]
     },
-    locationAdress: {
+    locationAddress: {
       
       street: { type: String, required: true },
       streetNumber: { type: String, required: true },
--- routes/index.js
+++ routes/index.js
@@ -5,18 +5,25 @@ var router = express.Router();
 const Listing = require("./../models/ListingModel");
 
 //Set up routers here
+const loginRouter = require("./auth");
+router.use("/login", loginRouter);
 
 //Get homepage with all listings 
 router.get('/', function(req, res, next) {
-  res.render('index')
+
+  Listing.find() // Method to render all listings available
+    .then( (data) => {
+      console.log('data found');
+      res.render('index', {data});
+    })
+    .catch( (err) => console.log(err));
 });
 
-  // Listing.find()
-  //   .then( (data) => {
-  //     res.render('index', {data});
-  //   })
-  //   .catch( (err) => console.log(err));
 
-
+//Helper function to check if user is logged in
+function isLoggedIn(req, res, next) {
+  if (req.session.currentUser) next();
+  else res.render('index');
+}
 
 module.exports = router;
