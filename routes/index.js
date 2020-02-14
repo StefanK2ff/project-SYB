81ac1bea36fda503b2438fabf1d004931df44fb7
jphrubant <jphrubant@gmail.com>
2020-02-14 12:45:44 +0100

    added route to log-in page

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ const Listing = require("./../models/ListingModel");
 const loginRouter = require("./auth");
 router.use("/log-in", loginRouter);
 
-//Get homepage with all listings 
+//Get route displaying all listings
 router.get('/', function(req, res, next) {
   Listing.find() // Method to render all listings available
     .then( (data) => {
@@ -17,10 +17,17 @@ router.get('/', function(req, res, next) {
     .catch( (err) => console.log(err));
 });
 
+// Get route directing to /bookings, if user is logged in
 router.get("/bookings", isLoggedIn, (req, res, next) => {
    res.render("bookings");
-})
+});
 
+//Get route to /log-in page 
+router.get("/log-in", (req, res, next) => {
+  res.render("log-in");
+});
+
+//helper function to check if user is logged in
 function isLoggedIn(req, res, next) {
   if (req.session.currentUser) next();
   else res.redirect("/login");
