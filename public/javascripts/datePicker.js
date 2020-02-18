4cf5ad9fb88f4da09df26ceb61675be106aec56d
Stefan Berk <stefan@k2ff.de#>
2020-02-18 15:43:28 +0100

    multi date picker works, now casting problem

---
+++ public/javascripts/datePicker.js
@@ -0,0 +1,21 @@
+const dateInput = document.querySelector('input[name="notAvailableDates"]');
+
+const datepicker = new Datepicker(dateInput, {
+    autohide: false,
+    calendarWeeks: false,
+    clearBtn: false,
+    dateDelimiter: ",",
+    // Array of date strings or a single date string formatted in the given date format.
+    datesDisabled: [],
+    disableTouchKeyboard: true,
+    format: "yyyy-mm-dd",
+    language: 'en',
+    maxNumberOfDates: 0,
+    minDate: Date.now(),
+    orientation: 'auto',
+    showOnFocus: true,
+    startView: 0,
+    todayBtn: true,
+    todayBtnMode: 0,
+    todayHighlight: true,
+});
