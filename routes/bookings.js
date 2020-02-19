29bc578b2d01dc4bdce1490bda213811900808a6
Stefan Berk <stefan@k2ff.de#>
2020-02-19 11:26:25 +0100

    updated variable names and primise handler

--- routes/bookings.js
+++ routes/bookings.js
@@ -67,89 +67,90 @@ router.get("/", (req, res) => {
     let user = false; // 
     if(req.session.currentUser){ // checker if user is logged in to display correct navbar
         user = req.session.currentUser //
-    }
-    // find all bookings, that the user reqested to others and are pending and in the future
-    const prom1 = Bookings.find({
+    
+        // find all bookings, that the user reqested to others and are pending and in the future
+        const prom1 = Bookings.find({
+                borrowerId: req.session.currentUser._id,
+                status: "pending",
+                bookingStart: {$gte: new Date()}
+            })
+            .then((pendingBookings) => { 
+                return {pendingBookings}
+            })
+            .catch((err) => console.log(err));
+
+        // find all bookings, that the user reqested, are accepted and upcoming
+        const prom2 = Bookings.find({
             borrowerId: req.session.currentUser._id,
-            status: "pending",
+            status: "accepted",
             bookingStart: {$gte: new Date()}
-        })
-        .then((madeBookings) => { 
-            return {pendingBookings}
-        })
-        .catch((err) => console.log(err));
+            })
+            .then((upcomingBookings) => { 
+                return {upcomingBookings}
+            })
+            .catch((err) => console.log(err));
 
-    // find all bookings, that the user reqested, are accepted and upcoming
-    const prom1 = Bookings.find({
-        borrowerId: req.session.currentUser._id,
-        status: "accepted",
-        bookingStart: {$gte: new Date()}
-    })
-    .then((madeBookings) => { 
-        return {madeBookings}
-    })
-    .catch((err) => console.log(err));
+        // find all bookings, that the user reqested to others that are declined or in the past (request archive)
+        const prom3 = Bookings.find({
+            borrowerId: req.session.currentUser._id,
+            $or: [{status: "declined"}, {bookingStart: {$lt: new Date()}}]
+            })
+            .then((requestArchive) => { 
+                return {requestArchive}
+            })
+            .catch((err) => console.log(err));
 
-    // find all bookings, that the user reqested to others that are declined or in the past (request archive)
-    const prom1 = Bookings.find({
-        borrowerId: req.session.currentUser._id,
-        $or: [{status: "declined"}, {bookingStart: {$lt: new Date()}}]
-    })
-    .then((madeBookings) => { 
-        return {madeBookings}
-    })
-    .catch((err) => console.log(err));
+        // find all the booking request , which are pending and are addressed to current user
+        const prom4 = Bookings.find({
+                ownerId: req.session.currentUser._id,
+                status: "pending"
+            })
+            .then((incomingBookings) => {
+                return {incomingBookings}
+            })
+            .catch((err) => console.log(err));
 
-    // find all the booking request , which are pending and are addressed to current user
-    const prom2 = Bookings.find({
+        // find all the booking request, which are accepted and are addressed to current user
+        const prom5 = Bookings.find({
             ownerId: req.session.currentUser._id,
-            status: "pending"
-        })
-        .then((incomingBookings) => {
-            return {incomingBookings}
-        })
-        .catch((err) => console.log(err));
+            status: "accepted"
+            })
+            .then((upcomingShares) => {
+                return {upcomingShares}
+            })
+            .catch((err) => console.log(err));
 
-    // find all the booking request, which are accepted and are addressed to current user
-     const prom2 = Bookings.find({
-        ownerId: req.session.currentUser._id,
-        status: "accepted"
-    })
-    .then((incomingBookings) => {
-        return {incomingBookings}
-    })
-    .catch((err) => console.log(err));
-
-    // find all the booking request, which are declined and are addressed to current user
-    const prom2 = Bookings.find({
-        ownerId: req.session.currentUser._id,
-        status: "declined"
-    })
-    .then((incomingBookings) => {
-        return {incomingBookings}
-    })
-    .catch((err) => console.log(err));
+        // find all the booking request, which are declined and are addressed to current user
+        const prom6 = Bookings.find({
+            ownerId: req.session.currentUser._id,
+            status: "declined"
+            })
+            .then((sharerArchive) => {
+                return {sharerArchive}
+            })
+            .catch((err) => console.log(err));
 
 
-    Promise.all([prom1, prom2])
-        .then((data) => {
-            let messageHandler = req.query.msg
-            switch (messageHandler) {
-                case "success":
-                    message = "Your booking request was sent to the owner!";
-                    break;
-                case "accepted":
-                    message = "The booking was accepted and the answer was sent to the Requester";
-                    break;
-                case "declined":
-                    message = "The request was declined and the answer was sent!";
-                    break;
-                default:
-                    message = null;
-            }
-            res.render("../views/bookings.hbs", {data, message, user});
-        })
-        .catch((err) => console.log(err));
+        Promise.all([prom1, prom2, prom3, prom4, prom5, prom6],)
+            .then((data) => {
+                let messageHandler = req.query.msg
+                switch (messageHandler) {
+                    case "success":
+                        message = "Your booking request was sent to the owner!";
+                        break;
+                    case "accepted":
+                        message = "The booking was accepted and the answer was sent to the Requester";
+                        break;
+                    case "declined":
+                        message = "The request was declined and the answer was sent!";
+                        break;
+                    default:
+                        message = null;
+                }
+                res.render("../views/bookings.hbs", {data, message, user});
+            })
+            .catch((err) => console.log(err));
+    } else {req.redirect("sign-up")}
 })
 
 module.exports = router;
